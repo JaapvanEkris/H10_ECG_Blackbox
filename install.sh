a03d8229e939bd8c76d1a23f62aef10b17045654
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2026-08-15 17:13:46 +0200

    Addition of the recorder

--- install.sh
+++ install.sh
@@ -4,6 +4,29 @@ mkdir h10box && cd h10box
 uv init
 uv python pin 3.14
 uv add bleakheart
+# add the HR belt to the trusted devices via bluetoothctl
+bluetoothctl
+power on
+agent on
+default-agent
+scan on
+pair XX:XX:XX:XX:XX:XX
+trust XX:XX:XX:XX:XX:XX
+scan off
+exit
+# This exits bluetoothctl
+
 # Testing the readout of the HR Belt
 uv run python smoketest1.py
 uv run python smoketest2.py
+
+# Set up the database location
+sudo mkdir -p /var/lib/h10box
+sudo chown $USER:$USER /var/lib/h10box
+
+# run the recorder
+uv run python smoketest2.py
+
+# install sqlite bash command and check database content
+sudo apt install sqlite3
+sqlite3 /var/lib/h10box/sessions.db "SELECT session_id, COUNT(*), SUM(n) FROM ecg_frames GROUP BY session_id;"
